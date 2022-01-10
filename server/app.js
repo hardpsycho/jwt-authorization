0d428c39efef4f7f8e6aa801e5542a9e98ab3be7
hardpsycho <harddeveloper@outlook.com>
2022-01-11 01:27:48 +0300

    added router

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+const router = require("./router/Router")
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
+app.use("/api", router)
 
 module.exports = app
---
+++ server/router/Router.js
@@ -0,0 +1,12 @@
+const {Router} = require("express")
+
+const router = new Router()
+
+router.post("/registration", )
+router.post("/login", )
+router.post("/logout", )
+router.post("/activate/:link", )
+router.post("/refresh", )
+router.post("/users", )
+
+module.exports = router
