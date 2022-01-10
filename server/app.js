348aeaec7f8d0d99c4f4279acb7e5be9f5bde308
hardpsycho <harddeveloper@outlook.com>
2022-01-11 01:22:45 +0300

    added models: User, Token

--- server/app.js
+++ server/app.js
@@ -1,5 +1,11 @@
 const express = require("express")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
 const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
 
 module.exports = app
