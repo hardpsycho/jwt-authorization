3e697451b3b1565307ec4a4b4e09bffc0ef148cc
hardpsycho <harddeveloper@outlook.com>
2022-01-11 00:23:27 +0300

    create mongoDB cluster and connected

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const app = require("./app")
+const mongoose = require("mongoose")
 require("dotenv").config()
 
 const PORT = process.env.PORT || 5005
 
 const start = async () => {
     try {
+        await mongoose.connect(process.env.DB_URL)
         app.listen(PORT, () => {
             console.log(`Server started on ${PORT} port`)
         })
