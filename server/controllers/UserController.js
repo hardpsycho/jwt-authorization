998034c6282bb30ce355d5590264df91bc9020a1
hardpsycho <harddeveloper@outlook.com>
2022-01-11 01:51:15 +0300

    added controller and services

---
+++ server/controllers/UserController.js
@@ -0,0 +1,51 @@
+class UserController {
+    registration = async (req, res, next) => {
+        try {
+
+        } catch (e) {
+
+        }
+    }
+
+    login = async (req, res, next) => {
+        try {
+
+        } catch (e) {
+
+        }
+    }
+
+    logout = async (req, res, next) => {
+        try {
+
+        } catch (e) {
+
+        }
+    }
+
+    activate = async (req, res, next) => {
+        try {
+
+        } catch (e) {
+
+        }
+    }
+
+    refresh = async (req, res, next) => {
+        try {
+
+        } catch (e) {
+
+        }
+    }
+
+    getUsers = async (req, res, next) => {
+        try {
+            res.json("test")
+        } catch (e) {
+
+        }
+    }
+}
+
+module.exports = new UserController()
--- server/router/Router.js
+++ server/router/Router.js
@@ -1,12 +1,13 @@
 const {Router} = require("express")
+const userController = require("../controllers/UserController")
 
 const router = new Router()
 
-router.post("/registration", )
-router.post("/login", )
-router.post("/logout", )
-router.post("/activate/:link", )
-router.post("/refresh", )
-router.post("/users", )
+router.post("/registration", userController.registration)
+router.post("/login", userController.login)
+router.post("/logout", userController.logout)
+router.post("/activate/:link", userController.activate)
+router.post("/refresh", userController.refresh)
+router.post("/users", userController.getUsers)
 
 module.exports = router
