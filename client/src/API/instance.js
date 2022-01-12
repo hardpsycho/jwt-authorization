1846611f23f3c03b63442653b4db47d39abe3657
hardpsycho <harddeveloper@outlook.com>
2022-01-12 10:38:40 +0300

    added response interceptor

--- client/src/API/instance.js
+++ client/src/API/instance.js
@@ -10,4 +10,21 @@ instance.interceptors.request.use( config => {
     return config
 })
 
+instance.interceptors.response.use( config => {
+    return config
+}, async error => {
+    const originalRequest = error.config
+    if(error.response.status == 401 && !error.config?._isRetry) {
+        originalRequest._isRetry = true
+        try {
+            const response = await axios.get("http://localhost:5000/api/refresh", {withCredentials: true})
+            localStorage.setItem("token", response.data.userToken)
+            return instance.request(originalRequest)
+        } catch (e) {
+            console.log("Пользователь не авторизован")
+        }
+    }
+    throw error
+})
+
 export default instance
